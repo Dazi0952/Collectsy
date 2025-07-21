a06c24621438534b1e61ac0c7d2c2912005ab2c4
Dazi0952 <dazii952@gmail.com>
2025-07-21 13:51:08 +0200

    Add runtime check for environment variables

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -5,6 +5,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("FATAL ERROR: Supabase URL or Anon Key is not defined.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
